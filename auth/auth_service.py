558de719b614bef8553d969f140828434b3739ae
uma <uma.shankar@og.com>
2026-08-02 15:16:26 +0530

    fixed changes in our files like dockerfiles,requirement

--- auth/auth_service.py
+++ auth/auth_service.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
-import os
-
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "db"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", "root"),
-        database=os.getenv("DB_NAME", "digital_library")
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
 
 @app.route("/signup", methods=["POST"])
@@ -41,5 +42,5 @@ def signin():
         return jsonify({"message": "Invalid credentials"}), 401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
